c945f5aec17f2edeb0c886352f8e882d81784a93
blackALT <{ID}+{username}@users.noreply.github.com>
2024-02-11 23:17:11 -0300

    change buttons options

--- src/views/buttons.js
+++ src/views/buttons.js
@@ -9,7 +9,7 @@ function menuPrincipal() {
         Markup.button.callback('📢 Dicas', 'tips')
     ], [
         Markup.button.callback('🚨 Ajuda', 'help'),
-        Markup.button.callback('❓ FAQ', 'faq')
+        Markup.button.url('🔗 Web API', 'google.com'),
     ], [
         Markup.button.callback('📞 Contact Us', 'contact'),
         Markup.button.callback('❌ Sair', 'quit')
@@ -17,6 +17,24 @@ function menuPrincipal() {
     return mainMenu;
 }
 
+function startMenu() {
+    const mainMenu = [
+        Markup.button.callback('🏡 Iniciar', 'start')
+    ]
+    return mainMenu
+}
+
+function retornoBuscaError() {
+    const mainMenu = [
+        Markup.button.callback('🔍 Buscar novamente', 'search'),
+        Markup.button.callback('🎣 Nova Analise', 'new')
+    ]
+    return mainMenu
+
+}
+
 module.exports = {
-    menuPrincipal
+    menuPrincipal,
+    startMenu,
+    retornoBuscaError
 }
