53987bf4627ac17b42e2105aa99d8dcc021436eb
blackALT <{ID}+{username}@users.noreply.github.com>
2024-02-13 22:23:05 -0300

    Messages FAQ and about

---
+++ src/views/messages.js
@@ -0,0 +1,51 @@
+const wellcomeMessage = `
+✻ ═══════ •🏡• ═══════ ✼
+    
+Bem-vindo(a) ao URL Watcher Bot!
+
+Comandos disponíveis:
+`;
+
+const helpMessage = `
+✻ ═══════ •💊• ═══════ ✼
+*AJUDA*
+
+1️⃣ *Como usar este bot?*
+
+TODO
+
+TODO
+
+TODO
+
+*SOBRE O PROJETO*
+
+    1️⃣ Como esse projeto funciona?
+
+    Esse bot está integrado à API URL Watcher. Uma API REST desenvolvidaUm projeto acadêmico que tem como objetivo
+    verificar
+    
+    2️⃣ Como esse projeto foi criado?
+
+    A solução surgiu como uma proposta para um trabalho de conclusão de curso baseada numa arquitetura para verificação de URLs maliciosas que conta com servidores virtuais e plataformas de código aberto, como Telegraf (chatbot), Cuckoo Sandbox (análise) e MongoDB (armazenamento). 
+    
+    3️⃣ Como posso contribuir com o projeto?
+
+    Para saber mais sobre nossa página de documentação ou o repositório do projeto no GitHub.
+`;
+
+const contactMessage = `
+✻ ═══════ • 📞• ═══════ ✼
+
+1️⃣ Deseja falar com o desenvolvedor?
+
+    Envie um e-mail para js2bqdb8m@mozmail.com
+
+✻ ═══════ •🏡• ═══════ ✼
+`
+
+module.exports = {
+    helpMessage,
+    contactMessage,
+    wellcomeMessage
+}
---
+++ src/views/tipsMessages.js
@@ -0,0 +1,112 @@
+// ===============================================================
+// Author: Wanessa Souza
+// Email: js2bqdb8m@mozmail.com
+// GitHub: blackALT
+// ===============================================================
+
+const wifiDicas = `
+
+🟢 *Use redes Wi-Fi públicas com cautela:*
+
+📌 Evite acessar informações confidenciais, como contas bancárias, em redes Wi-Fi
+
+📌 Considere usar uma VPN (Virtual Private Network) para proteger seu tráfego 
+
+📌 Evite digitar informações confidenciais, como dados bancários, ao usar redes 
+
+`
+
+const senhasDicas = ` 
+🔴 *Crie senhas fortes e exclusivas:*
+
+📌 Use combinações de letras maiúsculas e minúsculas, números e símbolos.
+
+📌 Evite usar informações pessoais como datas de nascimento ou nomes de familiares
+
+📌 Não use a mesma senha para várias contas
+
+📌 Considere usar um gerenciador de senhas para manter suas senhas seguras e organizadas
+
+📌 Ative a autenticação de dois fatores (2FA) sempre que possível, adicionando uma camada extra de proteção.
+
+`
+const softwareDicas = `
+🟠 *Atualize seu software regularmente:*
+
+📌 Mantenha o sistema operacional, navegadores e aplicativos do seu computador e celular atualizados com as últimas versões.
+
+📌 As atualizações geralmente incluem patches de segurança para corrigir vulnerabilidades conhecidas.
+
+📌 Atualize o firmware de roteadores e outros dispositivos conectados à internet para fechar vulnerabilidades conhecida.
+
+`
+
+const linksDicas = `
+🔵 *Tenha cuidado com links e anexos suspeitos:
+
+📌 Não clique em links ou abra anexos de emails desconhecidos ou suspeitos.
+
+📌 Verifique se o remetente é confiável antes de clicar em qualquer link. 
+
+📌 Passe o mouse sobre links para ver o URL real antes de clicar.
+
+📌 Desconfie de e-mails, mensagens de texto ou ligações suspeitas que solicitem seus dados pessoais ou bancários.
+
+📌 Não forneça informações confidenciais a menos que tenha certeza da legitimidade da fonte.
+
+`
+
+const socialDicas = `
+🟡 *Seja cauteloso nas redes sociais:*
+
+📌 Evite compartilhar informações pessoais demais nas redes sociais.
+
+📌 Tenha cuidado com quem aceita como amigo e configure as configurações de privacidade para limitar o acesso ao seu perfil.
+
+`
+
+const backupDicas = `
+🟤 *Faça backups regularmente:*
+
+📌 Faça backups regulares de seus dados importantes em locais seguros, como discos rígidos externos ou serviços de armazenamento em nuvem.
+
+`
+
+const mobileDicas = `
+🟣 *Proteja seus dispositivos móveis:*
+
+📌 Configure senhas fortes para seus smartphones e tablets.
+
+📌 Tenha atenção especial ao baixar aplicativos de fontes desconhecidas.
+
+📌 Instale aplicativos de segurança e mantenha o sistema operacional atualizado.
+
+`
+
+const golpesDicas = `
+⚫️ *Fique atento a golpes online:*
+
+📌 Existem diversos tipos de golpes online, como esquemas de pirâmide, investimentos falsos e ofertas 'imperdíveis
+
+📌 Pesquise antes de investir em qualquer coisa e desconfie de promessas muito boas para ser verdade."
+
+`
+
+const educacaoDicas = `
+⚪️ *Eduque-se sobre segurança cibernética:*
+
+📌 Mantenha-se informado sobre as últimas ameaças cibernéticas e dicas de segurança: Leia artigos, assista a vídeos e participe de workshops para se proteger melhor online"
+
+`
+
+module.exports = {
+    wifiDicas,
+    senhasDicas,
+    softwareDicas,
+    linksDicas,
+    mobileDicas,
+    golpesDicas,
+    socialDicas,
+    educacaoDicas,
+    backupDicas
+}
